fd3b3def0dce88038bda73b43b8c0ef91a1ea4f1
Alberto <55956550+Alberto5577@users.noreply.github.com>
2020-08-28 20:16:23 +0200

    going on

---
+++ juego.js
@@ -0,0 +1,32 @@
+document.addEventListener('keydown', function(evento){
+if(evento.keyCode == 32){
+    console.log("salta");
+}
+})
+
+
+var ancho = 700;
+var alto = 300;
+
+function inicializa(){
+    
+}
+
+
+
+
+function borraCanvas(){
+
+}
+
+//-------------------------------------------
+//BUCLE PRINCIPAL
+
+var FPS = 10;
+setInterval(function(){
+    principal();
+},1000/10);
+
+function principal (){
+    
+}
